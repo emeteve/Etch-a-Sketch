cbc73f3fc9001e71bbd7eb0a1fe58fd4170b6833
emeteve <uwucatgirlmeow@gmail.com>
2022-07-28 13:55:56 -0400

    Add hover and click to change grid color

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ function makeBox(boxNum) {
     for (i = 1; i <= boxNum*boxNum; i++) {
         let boxes = document.createElement('div');
         grid.appendChild(boxes).id = 'box';    
+
+        boxes.addEventListener('mouseover', () => {
+            boxes.classList.add('blackHover');
+        })
+
+        boxes.addEventListener('mouseout', () => {
+            boxes.classList.remove('blackHover');
+        })
+
+        boxes.addEventListener('click', () => {
+            boxes.classList.add('blackClick');
+        })
     }
 }
 
