f77abc0b14fb029fb6e0c69eb7e4727847d2c673
emeteve <uwucatgirlmeow@gmail.com>
2022-07-29 14:00:36 -0400

    Add color function and buttons for grid size

--- js/script.js
+++ js/script.js
@@ -1,23 +1,56 @@
 const grid = document.getElementById('grid');
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 function makeBox(boxNum) {
+    grid.style.gridTemplateColumns = `repeat(${boxNum}, 1fr)`
+    
     for (i = 1; i <= boxNum*boxNum; i++) {
         let boxes = document.createElement('div');
-        grid.appendChild(boxes).id = 'box';    
 
-        boxes.addEventListener('mouseover', () => {
-            boxes.classList.add('blackHover');
+        boxes.addEventListener('mouseover', color);
+        boxes.addEventListener('mousedown', color);
+
+        boxes.addEventListener('mouseenter', () => {
+            boxes.classList.add('black');
         })
-
         boxes.addEventListener('mouseout', () => {
-            boxes.classList.remove('blackHover');
+            boxes.classList.remove('black');
         })
 
-        boxes.addEventListener('click', () => {
-            boxes.classList.add('blackClick');
-        })
+        grid.appendChild(boxes).id = 'box';    
     }
 }
 
-makeBox(16);
+function color(e) {
+    if (e.type === 'mouseover' && !mouseDown) {
+        return;
+    } else {
+        e.target.style.backgroundColor = `black`;
+    }
+}
 
+function removeAllChild(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function grid16() {
+    removeAllChild(grid);
+    makeBox(16);
+}
+
+function grid64() {
+    removeAllChild(grid);
+    makeBox(64);
+}
+
+function grid32() {
+    removeAllChild(grid);
+    makeBox(32);
+}
+
+grid16();
